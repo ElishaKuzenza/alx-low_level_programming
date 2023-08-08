bdc66d285287d5d813d89a7d54fd293bf3b34dac
ElishaKuzenza <elishakuzenza25@gmail.com>
2023-08-08 05:03:19 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,8 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	int i;
-	concat_index = 0, len = 0;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,19 +21,19 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] || s2[i]; i++)
+	for (index = 0; s1[index] || s2[index]; index++)
 		len++;
 
-	concat = malloc(sizeof(char) * len);
+	concat_str = malloc(sizeof(char) * len);
 
-	if (concat == NULL)
+	if (concat_str == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i]; i++)
-		concat[concat_index++] = s1[i];
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
 
-	for (i = 0; s2[i]; i++)
-		concat[concat_index++] = s2[i];
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
 
-	return (concat);
+	return (concat_str);
 }
